8c504b1ab929714bf31230485b3cae9774fe9deb
LiveEkansh <83337805+LiveEkansh@users.noreply.github.com>
2021-09-03 12:14:32 +0530

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -136,27 +136,24 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Ekansh and I enjoy creating things that live on the internet. My
+              interest in web development started back in 2020 when the pandemic started.
+              All institutions closed and all i had was my pc and the internet. So i started programming!
+              From discord bots to websites , this pandemic did help me in learning :)
             </p>
 
             <p>
-              Fast-forward to today, and I've had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
+              Fast-forward to today, and I own a{' '}
+              <a href="https://discord.gg/vfqFtKvd">good discord server</a>,{' '}
               <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
               main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              experiences. </a>
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
+              I also am building an app by which one can theme their spotify just like {' '}
+              <a href="https://github.com/khanhas/spicetify-cli/discussions/578">
+                spicetify v2.
               </a>{' '}
               that covers everything you need to build a web app with the Spotify API using Node
               &amp; React.
