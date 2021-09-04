e8e2a9143d015c5dfbb5857088d11e3b2c560ced
Ekansh Sharma <83337805+LiveEkansh@users.noreply.github.com>
2021-09-04 22:54:34 +0530

    Update discord.js

--- src/components/icons/discord.js
+++ src/components/icons/discord.js
@@ -11,7 +11,9 @@ const IconDiscord = () => (
     strokeLinejoin="round"
     className="feather feather-discord">
     <title>Discord</title>
-    <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+     path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+    <rect x="2" y="9" width="4" height="12"></rect>
+    <circle cx="4" cy="4" r="2"></circle>
   </svg>
 );
 
