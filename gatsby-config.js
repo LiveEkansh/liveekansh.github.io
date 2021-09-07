22d6f11de90c1d57ed22313ef2e54e4119412dde
Ekansh Sharma <83337805+LiveEkansh@users.noreply.github.com>
2021-09-07 13:48:04 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'Live Ekansh is a student who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://ekansh.netlify.app', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    image: '/og.jpg', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
   plugins: [
